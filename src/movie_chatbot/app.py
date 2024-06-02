b917820a9399521c73d48124aa48605ccbaaa736
Stephanie Horng <sh390@duke.edu>
2024-06-02 17:05:05 -0400

    update app with chat UI and handle exception from endpoint

--- src/movie_chatbot/app.py
+++ src/movie_chatbot/app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 from llama_index.llms.llamafile import Llamafile
 
+# The implementation is largely based off of
+# https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps
+
+
+def initialize():
+    st.title("Movie Chatbot")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
 
 @st.cache_resource
 def load_model():
@@ -8,19 +24,36 @@ def load_model():
     return llm
 
 
-def retrieve_answer_and_output(model, prompt):
-    resp = model.complete(prompt)
-    st.markdown(resp)
+def chat(model):
+    # React to user input
+    if prompt := st.chat_input("What would you like to know?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        response = retrieve_from_model(model, prompt)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+def retrieve_from_model(model, prompt):
+    try:
+        resp = model.complete(prompt)
+    except Exception:  # pylint: disable=broad-exception-caught
+        resp = "I'm sorry. The query took too long to run. Please try another query instead."
+    return resp
 
 
 def run():
-    st.title("Movie Chatbot")
-
     model = load_model()
 
-    prompt = st.chat_input("What would you like to know?")
-    if prompt:
-        retrieve_answer_and_output(model, prompt)
+    initialize()
+
+    chat(model)
 
 
 if __name__ == "__main__":
